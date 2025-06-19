717427b35e741dd348b64c50310d99a504677df4
Mhmmddhapa <mhmmddhapa@gmail.com>
2025-06-20 05:19:41 +0700

    Stop tracking backend/config/db.js

--- backend/config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect(
-      "mongodb+srv://nyxsyncio:DhapaSync2004@cluster0.mu5ev5y.mongodb.net/OnlineShopDhap"
-    ). then(() => console.log("DB Connect")
-    );
-}
